7e672f9ab02ff525595101d75a9d99206a3ba7c0
Olena Reukova <kronverk01@gmail.com>
2023-05-13 14:18:17 +0100

    structured the Components
    added the structure into each component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
-  return <div> App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
